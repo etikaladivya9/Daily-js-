125e3ba54a9e9f68c88d94683e1be21698968953
etikaladivya9 <etikaladivya9@email.com>
2026-02-06 23:34:27 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,47 @@
+//operators
+//it is used to perform some operations twp operands\
+//1.Binary operators
+//a.arthmetic
+//b.assignment
+//c.comparsion /relational oparator
+//2.unary  operators
+//a.increment
+//b.decrement
+//ternaey operators
+//conditional statements
+
+//arthematic(+,-,*,/,%,**)
+let a=10
+let b=20
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+//assignment(=,+=,-=,/=,%=,*=)
+
+let c=22
+d=c+10
+
+console.log(d)
+console.log(c+d)
+//relational oparator(>,<,==,===,!=,<=,>=)
+
+
+//unary  operators(++,--)
+//preincrement
+//postincrement
+//predecrement
+//postdecrement
+
+let x=10
+
+console.log(++x);
+console.log(X++);
+console.log(--x)
+console.log(x--)
+
+
