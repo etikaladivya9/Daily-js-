a29ab3d6804253028eb68dbdafcbd4b26b1a8ca4
etikaladivya9 <etikaladivya9@email.com>
2026-02-08 23:11:07 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,58 @@
+//for
+//while
+//do while
+//for of
+//for in
+
+
+
+//for(init;condition;increment /decrement){
+//
+//}
+
+
+
+// for(let i=1; i<=limit; i++){
+//     console.log(i);
+// }
+
+// for(let i=limit; i>=1; i--){
+//     console.log(i);
+// }
+//  let i=limit
+//  while (i>=1){
+
+// console.log(i)
+//      i--;
+//  }
+//  let i=0
+// do {
+//     console.log(i)
+//     i++;
+
+// } while(1<=10)
+
+let arr=[1,2,3,4,5,6]
+// for (let i=0;i<arr.length;i++){
+// console.log(arr[i])
+// }
+
+// for (item of arr){
+//     console.log(item);
+// }
+// let obj={
+// name:'abc',
+// age:11
+// }
+
+
+// let o=Object.keys(obj);
+// for(i of o){
+//     console.log(i)
+// }
+//  for (i in obj){
+//      console.log(i)
+//  }
+let name1='xyz'
+console.log(name1[0]);
+
