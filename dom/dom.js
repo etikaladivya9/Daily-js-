6d39d1d35cdeb809accb497a6bdc998913e527f9
etikaladivya9 <etikaladivya9@gmail.com>
2026-04-08 20:12:52 +0530

    Dom

---
+++ dom/dom.js
@@ -0,0 +1,38 @@
+// let a1 = document.getElementById("divya");
+// a1.style.backgroundColor = "red";
+
+// let b2 = document.getElementsByClassName("text");
+// for(let i=0;i<b2.length;i++){
+//     b2[i].style.backgroundColor = "pink"
+// }
+
+// let tag = document.getElementsByTagName("h4")
+// tag[2].style.backgroundColor = "green"
+
+
+// let query = document.querySelector("h4");
+// // query.style.backgroundColor = "blue"
+// // query.innerText = "Divya"
+// query.innerHTML = `<h1>Marati</h1>`
+
+// let all = document.querySelectorAll("h4");
+// for(let i=0;i<all.length;i++){
+//     all[i].style.backgroundColor = "yellow"
+// }
+
+// let child = document.createElement("p");
+// child.innerText = "Marati";
+// a1.appendChild(child)
+
+
+let small = document.createElement("h3");
+small.innerText = "Divya";
+
+
+let big = document.createElement("p");
+big.innerText = "Chitti";
+// document.body.appendChild(small,big)
+document.body.append(small,big)
+
+
+
